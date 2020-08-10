e02650153797bb80b5aa1cbaff03542cbe41d2ed
xavier <xavier@codenectar.com>
2020-08-10 18:27:30 +0200

    added missing imports

--- wiz/core/telem/telem_sync.py
+++ wiz/core/telem/telem_sync.py
@@ -1,7 +1,7 @@
 import requests
 
-from wiz.core import utils
-from wiz.core.telem.ost import OperationState
+from wiz.core import utils, hub_client
+from wiz.core.telem.ost import OperationState, operation_states
 from wiz.core.wiz_app import wiz_app
 from wiz.serializers import operation_state_ser
 
@@ -15,6 +15,6 @@ def upload_operation_outcome(op_state: OperationState, delete=False):
   install_uuid = wiz_app.install_uuid
   serialized_outcome = operation_state_ser.serialize(op_state)
   ep = f'/api/cli/installs/{install_uuid}/operation_outcomes'
-  resp = hub_client.post(ep, json=dict(data=serialized_outcome))
+  resp = hub_client.post(ep, dict(data=serialized_outcome))
   print(resp)
   return True
