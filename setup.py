b3c5b1a1d86532e53c24330584092fb6d30a2067
xavier <xavier@codenectar.com>
2020-09-02 15:46:26 +0200

    walked out of dependency hell

--- setup.py
+++ setup.py
@@ -5,14 +5,29 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="nectwiz",
-  version="0.0.12",
+  version="0.0.16",
   author="Nectar Cloud Software",
   author_email="xavier@codenectar.com",
   description="App Wizard",
   long_description=long_description,
   long_description_content_type="text/markdown",
-  url="https://github.com/nectar-cs/wiz",
-  packages=setuptools.find_packages(),
+  url="https://github.com/nectar-cs/nectwiz",
+  packages = setuptools.find_packages(exclude=[
+    "nectwiz.tests.*", "nectwiz.tests"
+  ]),
+  install_requires=[
+    'flask>=1.1',
+    'flask-cors',
+    'k8-kat>=0.0.156',
+    'kubernetes>=10.0.1',
+    'requests',
+    'urllib3>=1.25',
+    'cachetools>=3.1',
+    'rsa>=4.0',
+    'validators',
+    'six>=1.12.0',
+    'inflection'
+  ],
   classifiers=[
     "Programming Language :: Python :: 3",
     "License :: OSI Approved :: MIT License",
