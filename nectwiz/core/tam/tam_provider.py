53c822c727bcb842cb6434af5b10907eacb96e5f
xavier <xavier@codenectar.com>
2020-09-10 12:29:06 -0400

    tam_provider.py fix

--- nectwiz/core/tam/tam_provider.py
+++ nectwiz/core/tam/tam_provider.py
@@ -1,6 +1,5 @@
 from nectwiz.core.core.config_man import config_man
 from nectwiz.core.tam.tamle_client import TamleClient
-
 from nectwiz.core.tam.tam_client import TamClient
 from nectwiz.core.tam.tami_client import TamiClient
 from nectwiz.core.tam.tams_client import TamsClient
@@ -8,11 +7,8 @@ from nectwiz.core.tam.tams_client import TamsClient
 
 def tam_client() -> TamClient:
   tam_type = config_man.tam()['type']
-  overrider = wiz_app.tam_client_override
 
-  if overrider:
-    return overrider()
-  elif tam_type == 'image':
+  if tam_type == 'image':
     return TamiClient()
   elif tam_type == 'server':
     return TamsClient()
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name="nectwiz",
-  version="0.0.210",
+  version="0.0.212",
   author="Nectar Cloud Software",
   author_email="xavier@codenectar.com",
   description="App Wizard",
@@ -18,7 +18,7 @@ setuptools.setup(
   install_requires=[
     'flask>=1.1',
     'flask-cors',
-    'k8kat>=0.0.201',
+    'k8kat>=0.0.203',
     'requests',
     'cachetools>=3.1',
     'redis>=3',
